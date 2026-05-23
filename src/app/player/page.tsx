d4905643d81b5b3aae2e420a461e4b04dafe2a09
Rajendra Kumar <contact.rajendrajakhar@gmail.com>
2026-05-23 21:46:26 +0530

    Fixed player size

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -48,7 +48,7 @@ function PlayerContent() {
   }
 
   return (
-    <main className="min-h-screen bg-black text-white p-4 max-w-md mx-auto relative overflow-hidden">
+    <main className="min-h-screen bg-black text-white p-4 max-w-md mx-auto relative overflow-hidden overflow-x-hidden w-screen">
 
       {/* Background Glow */}
       <div className="absolute top-0 left-0 w-72 h-72 bg-purple-600 blur-[120px] opacity-10 rounded-full"></div>
